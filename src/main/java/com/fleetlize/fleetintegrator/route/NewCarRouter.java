2d3a95d73e8a8b1a2d392dd9a37e935cbd54a918
Cezar Cruz <cezarcruz@outlook.com>
2020-03-22 11:18:49 -0300

    adding error handler and another stuffs

--- src/main/java/com/fleetlize/fleetintegrator/route/NewCarRouter.java
+++ src/main/java/com/fleetlize/fleetintegrator/route/NewCarRouter.java
@@ -1,17 +1,34 @@
 package com.fleetlize.fleetintegrator.route;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class NewCarRouter extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
+
+    errorHandler(
+        deadLetterChannel("direct:error")
+            .maximumRedeliveries(1)
+            .redeliveryDelay(1000)
+            .useOriginalMessage()
+    );
+
     from("jms:queue:car-creation")
-        .setProperty("plate").jsonpath("$.plate")
+        .setProperty("plate", jsonpath("$.plate"))
         .log("${header.plate}")
-        .serviceCall()
         .end();
+
+    from("direct:log3")
+        .routeId("loggerRoute3")
+        .log("${body}")
+        .setProperty("nome", jsonpath("$.nome"))
+        .setProperty("idade", jsonpath("$.idade"))
+        .log("------------------------------")
+        .log("${exchangeProperty[nome]}")
+        .log("${exchangeProperty[idade]}");
+
   }
 }
